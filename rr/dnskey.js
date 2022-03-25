8efb649ee4e98492da011fd0c43aaae8df48bc5c
Matt Simerson <matt@tnpi.net>
2022-03-25 16:40:21 -0700

    dnskey: fix copy/paste errors

--- rr/dnskey.js
+++ rr/dnskey.js
@@ -1,7 +1,7 @@
 
 const RR = require('./index').RR
 
-class DS extends RR {
+class DNSKEY extends RR {
   constructor (opts) {
     super(opts)
   }
@@ -10,28 +10,30 @@ class DS extends RR {
   setFlags (val) {
     // a 2 octet Flags Field
     // the possible values are: 0, 256, and 257 RFC 4034
-    if (![ 0, 256, 257 ].includes(val)) throw new Error(`DS: flags invalid, see ${this.getRFCs()}`)
+    if (![ 0, 256, 257 ].includes(val)) throw new Error(`DNSKEY: flags invalid, see ${this.getRFCs()}`)
 
     this.set('flags', val)
   }
 
   setProtocol (val) {
+    // 1 octet
     // The Protocol Field MUST be represented as an unsigned decimal integer with a value of 3.
-    if (![ 3 ].includes(val)) throw new Error(`DS: protocol invalid, see ${this.getRFCs()}`)
+    if (![ 3 ].includes(val)) throw new Error(`DNSKEY: protocol invalid, see ${this.getRFCs()}`)
 
     this.set('protocol', val)
   }
 
   setAlgorithm (val) {
+    // 1 octet
     // 1=RSA/MD5, 2=DH, 3=DSA/SHA-1, 4=EC, 5=RSA/SHA-1
     if (![ 1,2,3,4,5,253,254 ].includes(val))
-      throw new Error(`DS: algorithm invalid, see ${this.getRFCs()}`)
+      throw new Error(`DNSKEY: algorithm invalid, see ${this.getRFCs()}`)
 
     this.set('algorithm', val)
   }
 
   setPublickey (val) {
-    if (!val) throw new Error(`DS: publickey is required, see ${this.getRFCs()}`)
+    if (!val) throw new Error(`DNSKEY: publickey is required, see ${this.getRFCs()}`)
 
     this.set('publickey', val)
   }
@@ -78,4 +80,4 @@ class DS extends RR {
   // }
 }
 
-module.exports = DS
+module.exports = DNSKEY
