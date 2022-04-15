f04ed056643845b09c97d6f6dad385ace223b317
Matt Simerson <matt@tnpi.net>
2022-04-14 23:08:29 -0700

    add test/openpgpkey

---
+++ test/openpgpkey.js
@@ -0,0 +1,35 @@
+
+const base = require('./base')
+
+const OPENPGPKEY = require('../rr/openpgpkey.js')
+
+const validRecords = [
+  // {
+  //   owner    : 'example.com.',
+  //   ttl      : 3600,
+  //   class    : 'IN',
+  //   type     : 'OPENPGPKEY',
+  //   'public key': ``,
+  //   testB    : '',
+  //   testT    : '',
+  // },
+]
+
+const invalidRecords = [
+]
+
+describe('OPENPGPKEY record', function () {
+  base.valid(OPENPGPKEY, validRecords)
+  base.invalid(OPENPGPKEY, invalidRecords, { ttl: 3600 })
+
+  base.getDescription(OPENPGPKEY)
+  base.getRFCs(OPENPGPKEY)
+  base.getFields(OPENPGPKEY, [ 'public key' ])
+  base.getTypeId(OPENPGPKEY, 61)
+
+  base.toBind(OPENPGPKEY, validRecords)
+  // base.toTinydns(OPENPGPKEY, validRecords)
+
+  base.fromBind(OPENPGPKEY, validRecords)
+  // base.fromTinydns(OPENPGPKEY, validRecords)
+})
