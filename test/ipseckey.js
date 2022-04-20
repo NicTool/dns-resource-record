516ad5ff85c7d3f61cfc7749c96c0c6e53fd5f88
Matt Simerson <matt@tnpi.net>
2022-04-19 20:42:35 -0700

    tests(ipseckey): enabled to/from BIND tests

--- test/ipseckey.js
+++ test/ipseckey.js
@@ -14,7 +14,7 @@ const validRecords = [
     algorithm     : 2,
     gateway       : '192.0.2.38',
     publickey     : 'AQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==',
-    testB         : '38.2.0.192.in-addr.arpa. 7200 IN IPSECKEY 10 1 2 192.0.2.38 AQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==',
+    testB         : '38.2.0.192.in-addr.arpa.\t7200\tIN\tIPSECKEY\t10\t1\t2\t192.0.2.38\tAQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==\n',
   },
   {
     ...common,
@@ -24,7 +24,7 @@ const validRecords = [
     algorithm     : 2,
     gateway       : '.',
     publickey     : 'AQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==',
-    testB         : '38.2.0.192.in-addr.arpa. 7200 IN IPSECKEY 10 0 2 . AQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==',
+    testB         : '38.2.0.192.in-addr.arpa.\t7200\tIN\tIPSECKEY\t10\t0\t2\t.\tAQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==\n',
   },
   {
     ...common,
@@ -34,7 +34,7 @@ const validRecords = [
     algorithm     : 2,
     gateway       : '192.0.2.3',
     publickey     : 'AQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==',
-    testB         : '38.2.0.192.in-addr.arpa. 7200 IN IPSECKEY 10 1 2 . AQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==',
+    testB         : '38.2.0.192.in-addr.arpa.\t7200\tIN\tIPSECKEY\t10\t1\t2\t192.0.2.3\tAQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==\n',
   },
   {
     ...common,
@@ -44,7 +44,7 @@ const validRecords = [
     algorithm     : 2,
     gateway       : 'mygateway.example.com.',
     publickey     : 'AQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==',
-    testB         : '38.1.0.192.in-addr.arpa. 7200 IN     IPSECKEY 10 3 2 mygateway.example.com. AQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==',
+    testB         : '38.1.0.192.in-addr.arpa.\t7200\tIN\tIPSECKEY\t10\t3\t2\tmygateway.example.com.\tAQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==\n',
   },
   {
     ...common,
@@ -54,7 +54,7 @@ const validRecords = [
     algorithm     : 2,
     gateway       : '2001:0db8:0:8002::2000:1',
     publickey     : 'AQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==',
-    testB         : '0.d.4.0.3.0.e.f.f.f.3.f.0.1.2.0.1.0.0.0.0.0.2.8.b.d.0.1.0.0.2.ip6.arpa. 7200 IN IPSECKEY 10 2 2 2001:0DB8:0:8002::2000:1 AQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==',
+    testB         : '0.d.4.0.3.0.e.f.f.f.3.f.0.1.2.0.1.0.0.0.0.0.2.8.b.d.0.1.0.0.2.ip6.arpa.\t7200\tIN\tIPSECKEY\t10\t2\t2\t2001:0db8:0:8002::2000:1\tAQNRU3mG7TVTO2BkR47usntb102uFJtugbo6BSGvgqt4AQ==\n',
   },
 ]
 
@@ -71,9 +71,9 @@ describe('IPSECKEY record', function () {
   base.getFields(IPSECKEY, [ 'precedence', 'gateway type', 'algorithm', 'gateway', 'publickey' ])
   base.getTypeId(IPSECKEY, 45)
 
-  // base.toBind(IPSECKEY, validRecords)
+  base.toBind(IPSECKEY, validRecords)
   // base.toTinydns(IPSECKEY, validRecords)
 
-  // base.fromBind(IPSECKEY, validRecords)
+  base.fromBind(IPSECKEY, validRecords)
   // base.fromTinydns(IPSECKEY, validRecords)
 })
